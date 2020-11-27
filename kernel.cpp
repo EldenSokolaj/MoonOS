fa8ee4255298a79cf68746a090b07ffa4c5c5ee7
EldenSokolaj <56181940+EldenSokolaj@users.noreply.github.com>
2020-11-27 15:58:02 -0600

    Update kernel.cpp

--- kernel.cpp
+++ kernel.cpp
@@ -2,17 +2,14 @@
 #include "lib/logo.h"
 
 /*
-    Rune OS
-    Made by Athanasius Sokol
+    Moon OS
+        - A simple OS for x86_64 hardware -
+        - Made by Athanasius Sokol -
     Importent note: if binary goes past 0x20000 then you will need to update bootloader
 */
 
 extern "C" void kmain(){
-    float b = 0;
-    float a = b + 2;
-    b = a * 3;
     clrscr(7);
-    //line(10, 10, 20, 20, 0);
     set(10, 10, 0);
     for(;;);
 }
