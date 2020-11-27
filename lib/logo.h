81f37dbfa70400e6775c873c3ed73a7d2f5ab22f
EldenSokolaj <56181940+EldenSokolaj@users.noreply.github.com>
2020-11-27 15:59:20 -0600

    Update logo.h

--- lib/logo.h
+++ lib/logo.h
@@ -1,14 +1,6 @@
 #ifndef LOGO_H
 #define LOGO_H
-/*
-#include "io.h"
 
-void logo(){
-setColor((char)0x70);
-clrscr();
-const char *wolf = "                      .\n                     / V\\ \n                   / `  /\n                  <<   |\n                  /    |\n                /      |\n              /        |\n            /    \\  \\ /\n           (      ) | |\n   ________|   _/_  | |\n <__________\\______)\\__)\n\nWelcome to Moon OS\nPress any key to continue...\n";
-print((char*)wolf);
-return;
-}
-*/
+//Complete I/O functions before implimenting
+
 #endif
